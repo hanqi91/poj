5d224f03bdb92cb9451237d0868654dae079d189
韩琦 <hanqi91@qq.com>
2015-01-21 12:31:47 +0800

    modify indent

--- poj1562.cpp
+++ poj1562.cpp
@@ -16,44 +16,44 @@ void setNeighbor(int* grid, int ind, int m, int n, int pocket) {
 }
 
 int main() {
-	while (1) {
-		int m, n;
-		cin >> m >> n;
+    while (1) {
+        int m, n;
+        cin >> m >> n;
 
-		if (m == 0)
-			break;
+        if (m == 0)
+            break;
 
-		int* grid = new int[(m+2)*(n+2)];
-		for (int r = 0; r < m + 2; r++) {
-			for (int c = 0; c < n + 2; c++) {
-				grid[r*(n+2) + c] = -1;
-			}
-		}
+        int* grid = new int[(m+2)*(n+2)];
+        for (int r = 0; r < m + 2; r++) {
+            for (int c = 0; c < n + 2; c++) {
+                grid[r*(n+2) + c] = -1;
+            }
+        }
 
-		int cnt = 0;
-		while (cnt < m*n) {
-			int row = cnt/n + 1;
-			int col = cnt%n + 1;
-			char chr;
-			cin >> chr;
-			if (chr == '@') {
-				grid[row*(n+2) + col] = 0;
-			}
-			cnt++;
-		}
+        int cnt = 0;
+        while (cnt < m*n) {
+            int row = cnt/n + 1;
+            int col = cnt%n + 1;
+            char chr;
+            cin >> chr;
+            if (chr == '@') {
+                grid[row*(n+2) + col] = 0;
+            }
+            cnt++;
+        }
 
-		int pocket = 0;
-		for (int r = 1; r < m + 1; r++) {
-			for (int c = 1; c < n + 1; c++) {
-				if (grid[r*(n+2) + c] == 0) {
-					pocket++;
-					grid[r*(n+2) + c] = pocket;
-					int ind = r*(n+2) + c;
-					setNeighbor(grid, ind, m+2, n+2, pocket);
-				}
-			}
-		}
-		cout << pocket << endl;
-	}
+        int pocket = 0;
+        for (int r = 1; r < m + 1; r++) {
+            for (int c = 1; c < n + 1; c++) {
+                if (grid[r*(n+2) + c] == 0) {
+                    pocket++;
+                    grid[r*(n+2) + c] = pocket;
+                    int ind = r*(n+2) + c;
+                    setNeighbor(grid, ind, m+2, n+2, pocket);
+                }
+            }
+        }
+        cout << pocket << endl;
+    }
     return 0;
 }
